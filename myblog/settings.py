845581b433a81c31708204f6c5e2628e63a1e8ff
yuzeshan <shanyuze1029@163.com>
2015-04-27 23:35:25 +0800

    简单修改

--- myblog/settings.py
+++ myblog/settings.py
@@ -145,16 +145,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/').replace('\\','/')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "manager/static"),
+    STATIC_ROOT,
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/').replace('\\','/')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
-print STATIC_ROOT
+
 
 
 
